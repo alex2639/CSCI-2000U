58fea03fac7610704a23335ede4232a67cdd5b87
Alex <mr.e_000@hotmail.com>
2015-11-05 08:51:00 -0500

    Assignment 2: completed strip.sh

--- Assignments/Assignment-2/strip.sh
+++ Assignments/Assignment-2/strip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Alex Ly 100548033
 cat $3 | tail -n +$1 | head -n -$2 
-#head -n -$2 $3 removes the last $2 lines from file $3
-# |tail -n +$1 $3 removes the first $1 lines from the output of the left side of the pipe
+#| head -n -$2 removes the last $2 lines from the output of the left side of the pipe
+#| tail -n +$1 removes the first $1 lines from the concatenation of file $3
+#cat $3 concatenates file $3
